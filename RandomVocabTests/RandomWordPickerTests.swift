45a02531c03dcf5544d23f67c96f573c146b10d9
Kazi Mashry <kazimashry@gmail.com>
2024-07-30 11:54:16 +0600

    added tests

--- RandomVocabTests/RandomWordPickerTests.swift
+++ RandomVocabTests/RandomWordPickerTests.swift
@@ -9,52 +9,60 @@ import XCTest
 @testable import RandomVocab
 
 final class RandomWordPickerTests: XCTestCase {
-
+ 
     let wordList: [String] = ["one", "two", "three", "four", "five"]
 
-    class WordReader: AnyWordListReader {
+    class MockWordReader: AnyWordListReader {
+        func getWordList(from file: RandomVocab.FileNameContainer.File) -> [String]? {
+            wordList
+        }
+        
         let wordList: [String]?
         
         init(wordList: [String]? = nil) {
             self.wordList = wordList
         }
         
-        func getWordList() -> [String]? {
-            wordList
-        }
-        
         
     }
     
     func test_RandomWordPickerWithWordListContainingWords() {
-        let wordPicker: AnyRandomWordPicker = RandomWordPicker(wordListReader: WordReader(wordList: wordList))
+        let sut: AnyRandomWordPicker = RandomWordPicker(wordListReader: MockWordReader(wordList: wordList))
         
         for _ in 0..<20 {
-            if let randomWord = wordPicker.getWord() {
+            if let randomWord = sut.getWord(from: FileNameContainer.File(name: "", type: "")) {
                 XCTAssertTrue(wordList.contains(randomWord), "The picked word should be in the word list")
             }
         }
     }
     
     func test_RandomWordPickerWithZeroContainingWord() {
-        let wordPicker: AnyRandomWordPicker = RandomWordPicker(wordListReader: WordReader(wordList: []))
-        let randomWord = wordPicker.getWord()
+        let sut: AnyRandomWordPicker = RandomWordPicker(wordListReader: MockWordReader(wordList: []))
+        let randomWord = sut.getWord(from: FileNameContainer.File(name: "", type: ""))
         
         XCTAssertNil(randomWord, "It should retrun nil")
     }
     
     func test_RandomWordPickerWithNILWordList() {
-        let wordPicker: AnyRandomWordPicker = RandomWordPicker(wordListReader: WordReader(wordList: nil))
-        let randomWord = wordPicker.getWord()
+        let sut: AnyRandomWordPicker = RandomWordPicker(wordListReader: MockWordReader(wordList: nil))
+        let randomWord = sut.getWord(from: FileNameContainer.File(name: "", type: ""))
+        
+        XCTAssertNil(randomWord, "It should retrun nil")
+    }
+    
+    func test_RandomWordPickerWithNILWordListAfterGettingDataOnPreviousNotNILCall() {
+        let sut: AnyRandomWordPicker = RandomWordPicker()
+        let _ = sut.getWord(from: FileNameContainer.wordListCSV)
+        let randomWord = sut.getWord(from: FileNameContainer.File(name: "", type: ""))
         
         XCTAssertNil(randomWord, "It should retrun nil")
     }
     
     func test_RandomWordPickerWithCSV() {
-        let wordPicker: AnyRandomWordPicker = RandomWordPicker()
+        let sut: AnyRandomWordPicker = RandomWordPicker()
         
         for _ in 0..<20 {
-            let randomWord = wordPicker.getWord()
+            let randomWord = sut.getWord(from: FileNameContainer.wordListCSV)
             print(randomWord as Any)
             XCTAssertTrue(randomWord is String?, "Random word should be string")
         }

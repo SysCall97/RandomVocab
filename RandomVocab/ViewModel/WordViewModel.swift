e38fb735a06626848d04edf813fee9de4b095bea
Kazi Mashry <kazimashry@gmail.com>
2024-08-02 12:11:31 +0600

    added swiftData to the model for storing favourite words

--- RandomVocab/ViewModel/WordViewModel.swift
+++ RandomVocab/ViewModel/WordViewModel.swift
@@ -6,13 +6,17 @@
 //
 
 import Foundation
+import SwiftData
 
+@Model
 class WordViewModel {
+    var id: String
     let word: String
     let phonetics: APIResponseDataModel.Phonetic
     var meanings: [APIResponseDataModel.Meaning]
     
     init(from response: [APIResponseDataModel]) {
+        self.id = response.first!.word
         self.word = response.first?.word ?? ""
         self.phonetics = (response.first?.phonetics.first)!
         self.meanings = []

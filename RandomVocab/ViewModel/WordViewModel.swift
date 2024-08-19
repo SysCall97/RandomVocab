6912203320b4d7a093725d3d2ec09327c677a454
Kazi Mashry <kazimashry@gmail.com>
2024-08-19 07:14:30 +0600

    [UPDATE]::  - added view model to encorporate with model and view

--- RandomVocab/ViewModel/WordViewModel.swift
+++ RandomVocab/ViewModel/WordViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class WordViewModel {
+    let wordModel: WordModel
+    private(set) var isMarkedAsFavourite: Bool
+    private(set) var isPhoneticsHasAudibleFile: Bool = false
+    
+    init(with model: WordModel, isFavourite: Bool) {
+        self.wordModel = model
+        self.isMarkedAsFavourite = isFavourite
+        if let phonetics = model.phonetics {
+            if let _ = phonetics.audio {
+                self.isPhoneticsHasAudibleFile = true
+            }
+        }
+    }
+}

1779e997ba707f3ef9aeb7b115a2b9221dda1757
Kazi Mashry <kazimashry@gmail.com>
2024-08-23 21:10:53 +0600

    [UPDATE]::  - removed default UI test

--- RandomVocabUITests/RandomVocabUITestsLaunchTests.swift
+++ RandomVocabUITests/RandomVocabUITestsLaunchTests.swift
@@ -1,32 +1,32 @@
+////
+////  RandomVocabUITestsLaunchTests.swift
+////  RandomVocabUITests
+////
+////  Created by Kazi Mashry on 28/7/24.
+////
 //
-//  RandomVocabUITestsLaunchTests.swift
-//  RandomVocabUITests
+//import XCTest
 //
-//  Created by Kazi Mashry on 28/7/24.
+//final class RandomVocabUITestsLaunchTests: XCTestCase {
 //
-
-import XCTest
-
-final class RandomVocabUITestsLaunchTests: XCTestCase {
-
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
-
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-    }
-
-    func testLaunch() throws {
-        let app = XCUIApplication()
-        app.launch()
-
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
-
-        let attachment = XCTAttachment(screenshot: app.screenshot())
-        attachment.name = "Launch Screen"
-        attachment.lifetime = .keepAlways
-        add(attachment)
-    }
-}
+//    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+//        true
+//    }
+//
+//    override func setUpWithError() throws {
+//        continueAfterFailure = false
+//    }
+//
+//    func testLaunch() throws {
+//        let app = XCUIApplication()
+//        app.launch()
+//
+//        // Insert steps here to perform after app launch but before taking a screenshot,
+//        // such as logging into a test account or navigating somewhere in the app
+//
+//        let attachment = XCTAttachment(screenshot: app.screenshot())
+//        attachment.name = "Launch Screen"
+//        attachment.lifetime = .keepAlways
+//        add(attachment)
+//    }
+//}

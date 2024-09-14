054f308ee160d4f4cd68147f8b4496bf2b13f140
Kazi Mashry <kazimashry@gmail.com>
2024-09-14 12:57:54 +0600

    [UPDATE]::  - added table view  - make it expandable

--- RandomVocab/ViewControllers/FavouriteWordsVC/FavouriteWordsVC.swift
+++ RandomVocab/ViewControllers/FavouriteWordsVC/FavouriteWordsVC.swift
@@ -7,38 +7,89 @@
 
 import UIKit
 
+class TableViewModel {
+    let wordViewModel: WordViewModel
+    var isOpened: Bool
+    
+    init(wordViewModel: WordViewModel, isOpened: Bool = false) {
+        self.wordViewModel = wordViewModel
+        self.isOpened = isOpened
+    }
+}
+
 class FavouriteWordsVC: UIViewController {
     static let storyboardName = "FavouriteWordsVC"
     static let storyboardIdentifier = "FavouriteWordsVC"
     
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     var wordManager: AnyWordManager! {
         didSet {
             Task {
-                favWords = await wordManager.getFavouriteWords() ?? []
-                self.favWords.forEach { wvm in
-                    print(wvm.wordModel.word)
+                let favWords = await wordManager.getFavouriteWords() ?? []
+                favWords.forEach { wordViewModel in
+                    self.tableViewModels.append(TableViewModel(wordViewModel: wordViewModel))
                 }
+                tableView.reloadData()
             }
         }
     }
     
-    private var favWords = [WordViewModel]()
+    private var tableViewModels = [TableViewModel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.frame = CGRect(x: 0, y: 40, width: Int(self.view.bounds.width), height: Int(self.view.bounds.height))
     }
+}
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension FavouriteWordsVC: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        self.tableViewModels.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let section = tableViewModels[section]
+        
+        if section.isOpened {
+            return section.wordViewModel.wordModel.meanings.count + 1
+        }
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.numberOfLines = 0
+        if indexPath.row == 0 {
+            cell.textLabel?.text = self.tableViewModels[indexPath.section].wordViewModel.wordModel.word
+            cell.backgroundColor = .lightGray
+            return cell
+        } else {
+            cell.textLabel?.text = self.tableViewModels[indexPath.section].wordViewModel.wordModel.meanings[indexPath.row - 1].definitions.first?.definition
+            cell.backgroundColor = .white
+        }
+        
+        return cell
+    }
+    
+}
 
+extension FavouriteWordsVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.row == 0 {
+            tableViewModels[indexPath.section].isOpened = !tableViewModels[indexPath.section].isOpened
+            tableView.reloadSections([indexPath.section], with: .none)
+        }
+    }
 }
